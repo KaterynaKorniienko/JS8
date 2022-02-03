2082b98704b0fb65c4fe899b4f51fcbde62c72ae
KaterynaKorniienko <katerynakorniienko7@ukr.net>
2022-02-03 22:17:42 +0200

    first commit

---
+++ src/gallery-items.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    description: "Thor",
+    preview:
+      "https://cdn.britannica.com/73/182873-050-E1C686F4/Chris-Hemsworth-Thor-Thor-The-Dark-World.jpg",
+    original:
+      "https://cdn.britannica.com/73/182873-050-E1C686F4/Chris-Hemsworth-Thor-Thor-The-Dark-World.jpg",
+  },
+  {
+    description: "Iron Man",
+    preview:
+      "https://img5.goodfon.ru/wallpaper/nbig/b/f4/iron-man-toni-stark-zheleznyi-chelovek-mark-3.jpg",
+    original:
+      "https://img5.goodfon.ru/wallpaper/nbig/b/f4/iron-man-toni-stark-zheleznyi-chelovek-mark-3.jpg",
+  },
+  {
+    description: "Captain America",
+    preview:
+      "https://www.film.ru/sites/default/files/filefield_paths/chris_0.jpg",
+    original:
+      "https://www.film.ru/sites/default/files/filefield_paths/chris_0.jpg",
+  },
+  {
+    description: "Black Widow",
+    preview:
+      "https://cdn.vox-cdn.com/thumbor/4RkxUsOYgclni1EQt5uFuR81GZY=/0x0:1300x650/1200x800/filters:focal(421x134:629x342)/cdn.vox-cdn.com/uploads/chorus_image/image/67456060/black_widow_marvel.0.jpeg",
+    original:
+      "https://cdn.vox-cdn.com/thumbor/4RkxUsOYgclni1EQt5uFuR81GZY=/0x0:1300x650/1200x800/filters:focal(421x134:629x342)/cdn.vox-cdn.com/uploads/chorus_image/image/67456060/black_widow_marvel.0.jpeg",
+  },
+  {
+    description: "Hulk",
+    preview:
+      "https://reelfreak.files.wordpress.com/2014/05/hulk-still_avengers_660.jpg",
+    original:
+      "https://reelfreak.files.wordpress.com/2014/05/hulk-still_avengers_660.jpg",
+  },
+  {
+    description: "Hawkeye",
+    preview:
+      "https://cdn3.whatculture.com/images/2015/11/305BpQoq.jpg",
+    original:
+      "https://cdn3.whatculture.com/images/2015/11/305BpQoq.jpg",
+  },
+  {
+    description: "Doctor Strange",
+    preview:
+      "https://cdn.kanobu.ru/articles/pics/a829b929-b8d2-46f7-bcdd-05e391a98f0c.jpg",
+    original:
+      "https://cdn.kanobu.ru/articles/pics/a829b929-b8d2-46f7-bcdd-05e391a98f0c.jpg",
+  },
+  {
+    description: "Black Panther",
+    preview:
+      "https://www.cinematographe.it/wp-content/uploads/2017/02/3059020-blackpantherdeck.jpg",
+    original:
+      "https://www.cinematographe.it/wp-content/uploads/2017/02/3059020-blackpantherdeck.jpg",
+  },
+  {
+    description: "Ant Man and Wasp",
+    preview:
+      "http://kinowar.com/wp-content/uploads/2018/07/%D1%87%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA-%D0%BC%D1%83%D1%80%D0%B0%D0%B2%D0%B5%D0%B9-%D0%B8-%D0%BE%D1%81%D0%B0_06.jpg",
+    original:
+      "http://kinowar.com/wp-content/uploads/2018/07/%D1%87%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA-%D0%BC%D1%83%D1%80%D0%B0%D0%B2%D0%B5%D0%B9-%D0%B8-%D0%BE%D1%81%D0%B0_06.jpg",
+  },
+  {
+    description: "Winter Soldier",
+    preview:
+      "https://cdn.vox-cdn.com/thumbor/tr1F8BUCM9O1V0EMAcXv5tdTbPs=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/assets/4216747/captainamerica_wintersoldier7_1020.jpg",
+    original:
+      "https://cdn.vox-cdn.com/thumbor/tr1F8BUCM9O1V0EMAcXv5tdTbPs=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/assets/4216747/captainamerica_wintersoldier7_1020.jpg",
+  },
+  {
+    description: "Spider Man",
+    preview:
+      "https://imagesvc.meredithcorp.io/v3/jumpstartpure/image?url=https://static.onecms.io/wp-content/uploads/sites/6/2021/12/14/Spider-man-No-Way-Home_1.jpg&w=1280&h=720&q=90&c=cc",
+    original:
+      "https://imagesvc.meredithcorp.io/v3/jumpstartpure/image?url=https://static.onecms.io/wp-content/uploads/sites/6/2021/12/14/Spider-man-No-Way-Home_1.jpg&w=1280&h=720&q=90&c=cc",
+  },
+  {
+    description: "Scarlet Witch",
+    preview:
+      "https://i.ytimg.com/vi/Xx5u0qFUpJs/maxresdefault.jpg",
+    original:
+      "https://i.ytimg.com/vi/Xx5u0qFUpJs/maxresdefault.jpg",
+  },
+];
---
+++ src/script.js
@@ -0,0 +1,89 @@
+import gallery from "./gallery-items.js";
+let refs = {
+  ul: document.querySelector(".js-gallery"),
+  lightBox: document.querySelector(".js-lightbox"),
+  lightBoxImage: document.querySelector(".lightbox__image"),
+  lightBoxCloseButton: document.querySelector(
+    'button[data-action="close-lightbox"]'
+  ),
+  lightBoxOverlay: document.querySelector(".lightbox__overlay"),
+};
+let images = [];
+let index = 0;
+
+// Generate the gallery
+gallery.forEach((img) => {
+  let li = `
+  <li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${img.original}"
+    >
+      <img
+        class="gallery__image"
+        src="${img.preview}"
+        data-source="${img.original}"
+        alt="${img.description}"
+      />
+    </a>
+
+  </li>
+  `;
+  images.push(img.original);
+  refs.ul.insertAdjacentHTML("beforeend", li);
+});
+
+// Events
+refs.ul.addEventListener("click", open);
+refs.lightBoxCloseButton.addEventListener("click", close);
+refs.lightBoxOverlay.addEventListener("click", close);
+
+function open(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") return;
+  index = images.indexOf(evt.target.dataset.source);
+  window.addEventListener("keydown", keyAction);
+  imgUpdate(index);
+  refs.lightBox.classList.add("is-open");
+}
+
+// lightBoxImage update
+let imgUpdate = (idx) => (refs.lightBoxImage.src = images[idx]);
+
+// Close button
+function close() {
+  refs.lightBox.classList.remove("is-open");
+  refs.lightBoxImage.src = "";
+  window.removeEventListener("keydown", keyAction);
+}
+
+// Modal//
+let modalWindow = document.querySelector(".lightbox");
+const closeBtn = document.querySelector(".lightbox__button");
+const backdropRef = document.querySelector(".lightbox__overlay");
+let bigImgRef = document.querySelector(".lightbox__image");
+let idx;
+
+// Closing modal
+function closeModal() {
+  modalWindow.classList.remove("is-open");
+  window.removeEventListener("keydown", onWindowEvent);
+  bigImgRef.setAttribute("src", "");
+  bigImgRef.setAttribute("alt", "");
+  closeBtn.removeEventListener("click", closeModal);
+  backdropRef.removeEventListener("click", onBackDropClick);
+}
+// Key actions
+function keyAction(evt) {
+  if (evt.code === "Escape") close();
+  if (evt.code === "ArrowLeft") {
+    index -= 1;
+    index < 0 ? (index = images.length - 1) : index;
+  }
+  if (evt.code === "ArrowRight") {
+    index += 1;
+    index === images.length ? (index = 0) : index;
+  }
+  console.log(index);
+  imgUpdate(index);
+}
